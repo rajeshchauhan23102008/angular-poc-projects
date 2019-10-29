d7c7a67aafa1a343da95eb4c558f35eb7d0446aa
rajeshchauhan23102008 <rajesh.chauhan23102008@gmail.com>
2019-10-29 21:16:45 +0530

    adding default value to ngModel

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -4,9 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
-  styles:[`p { color: red}`]
+  styles: [
+    `
+      p {
+        color: red;
+      }
+    `
+  ]
 })
 export class AppComponent {
   title = 'does this really change';
-  name = '';
+  name = 'rajesh';
 }
